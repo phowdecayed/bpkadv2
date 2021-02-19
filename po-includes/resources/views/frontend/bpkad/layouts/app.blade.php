5f775c4418430d3a1afd023df57a601f3ada5ce5
phowdecayed <decayphewdance@gmail.com>
2021-02-19 10:35:58 +0700

    fix search form

--- po-includes/resources/views/frontend/bpkad/layouts/app.blade.php
+++ po-includes/resources/views/frontend/bpkad/layouts/app.blade.php
@@ -73,24 +73,23 @@
 								<img src="{{ asset('po-content/uploads/'.getSetting('logo')) }}" alt="">
 							</a>
 						</div>
-
-                        <div id="primary-menu-trigger">
-                            <svg class="svg-trigger" viewBox="0 0 100 100">
-                                <path d="m 30,33 h 40 c 3.722839,0 7.5,3.126468 7.5,8.578427 0,5.451959 -2.727029,8.421573 -7.5,8.421573 h -20"></path>
-                                <path d="m 30,50 h 40"></path>
-                                <path d="m 70,67 h -40 c 0,0 -7.5,-0.802118 -7.5,-8.365747 0,-7.563629 7.5,-8.634253 7.5,-8.634253 h 20"></path>
-                            </svg>
+                        <div class="header-misc">
+                            <div id="top-search" class="header-misc-icon">
+                                <a href="#" id="top-search-trigger"><i class="icon-line-search"></i><i class="icon-line-cross"></i></a>
+                            </div>
                         </div>
+
+						<div id="primary-menu-trigger">
+							<svg class="svg-trigger" viewBox="0 0 100 100"><path d="m 30,33 h 40 c 3.722839,0 7.5,3.126468 7.5,8.578427 0,5.451959 -2.727029,8.421573 -7.5,8.421573 h -20"></path><path d="m 30,50 h 40"></path><path d="m 70,67 h -40 c 0,0 -7.5,-0.802118 -7.5,-8.365747 0,-7.563629 7.5,-8.634253 7.5,-8.634253 h 20"></path></svg>
+						</div>
+
 			            <nav class="primary-menu with-arrows">
                                 <ul class="menu-container">
                                     @each(getTheme('partials.menu'), getMenus(), 'menu', getTheme('partials.menu'))
-                                    <div id="top-search" class="header-misc-icon">
-                                        <a href="#" id="top-search-trigger"><i class="icon-line-search"></i><i class="icon-line-cross"></i></a>
-                                    </div>
                                 </ul>
                         </nav>
                         <form class="top-search-form row" action="{{ url('search') }}" method="get">
-                            <input type="text" name="terms" class="form-control col-6" placeholder="Ketik &amp; Tekan Enter.." autocomplete="off">
+                            <input type="text" name="terms" class="form-control" placeholder="Ketik &amp; Tekan Enter.." autocomplete="off">
                         </form>
 				    </div>
                 </div>
